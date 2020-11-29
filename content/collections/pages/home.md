c68f5a8cf5ec00862ac521f533b7ff49332c4345
Niels Bergert <bergert@hotmail.de>
2020-11-29 18:49:10 +0100

    added Projekt anfang

---
+++ content/collections/pages/home.md
@@ -0,0 +1,9 @@
+---
+title: Home
+template: new
+author: 4aac0320-1b4c-4e9c-8f0f-9fb608069f16
+updated_by: 4aac0320-1b4c-4e9c-8f0f-9fb608069f16
+updated_at: 1606530260
+id: home
+---
+Hello World
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: false
+    {
+      content: [
+        './resources/**/*.antlers.html',
+        './resources/**/*.blade.php',
+        './content/**/*.md'
+      ]
+    },
+    important: true,
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+ }
